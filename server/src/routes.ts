6c14148eab14a766c2bfb0cc49b0dfcdc5a56ef3
master <gsuganame@gmail.com>
2020-07-05 16:14:56 -0300

    Application commit

---
+++ server/src/routes.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get("/items", itemsController.index);
+routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
+
+//index - listagem
+//show - unico registro
+//create - criação
+//update - atualizar registro
+//destroy - deletar um registro
+
+export default routes;
